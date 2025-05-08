be5f44f88eac45f1980ffa9340293c69f21ad158
dumpysmasher <the1kzg@gmail.com>
2025-05-08 14:13:35 -0400

    moved some includes

--- proj1.c
+++ proj1.c
@@ -14,6 +14,7 @@
 
 #if __linux
     #include <sys/syscall.h>
+    #define gettid() syscall(SYS_gettid)
 #elif defined(_WIN32) || defined(_WIN64)
     #include <windows.h>       // Or something like it. 
     #define gettid() GetCurrentThreadId()
