4aa41616fc76b8636d74aac4650ec2d8e2dc260c
dumpysmasher <the1kzg@gmail.com>
2025-05-07 23:09:17 -0400

    try this

--- proj1.c
+++ proj1.c
@@ -21,30 +21,40 @@
 void SIGINT_handler(){
 write(STDOUT_FILENO,"hi",2);
 }
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+
 void* proc(void* arg) {
-    int sum=0;
-    for(int i=0;i<11;i++){
-    pid_t tid = getpid();
-    sum=sum+i*tid;
-    //write(STDOUT_FILENO,"i",1);
-    sleep(1);
-    
+    int sum = 0;
+    for (int i = 0; i <= 10; i++) {
+        pid_t tid = syscall(SYS_gettid);
+        pid_t pid = getpid();
+        sum += i * tid;
+        char buffer[64];
+        int len = snprintf(buffer, sizeof(buffer), "TID: %d, PID: %d\n", tid, pid);
+        write(STDOUT_FILENO, buffer, len);
+
+        sleep(1);
     }
     
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
-signal(SIGINT, SIGINT_handler);
-pthread_t thread1;
-pthread_t thread2;
-pthread_t thread3;
-pthread_t thread4;
-
+    // sigset_t fullset;
+    // sigfillset(&fullset);
+    // pthread_sigmask(SIG_BLOCK, &fullset, NULL)
+    signal(SIGINT, SIGINT_handler);
+    pthread_t thread1;
+    pthread_t thread2;
+    pthread_t thread3;
+    pthread_t thread4;
+    
 pthread_create(&thread1, NULL, proc, NULL);
-pthread_create(&thread2, NULL, proc, NULL);
-pthread_create(&thread3, NULL, proc, NULL);
-pthread_create(&thread4, NULL, proc, NULL);
+    pthread_create(&thread2, NULL, proc, NULL);
+    pthread_create(&thread3, NULL, proc, NULL);
+    pthread_create(&thread4, NULL, proc, NULL);
 printf("%d\n",thread1);
 printf("%d\n",thread2);
 printf("%d\n",thread3);
@@ -56,6 +66,10 @@ pthread_join(thread3, NULL);
 pthread_join(thread4, NULL);
 
 
-// Creating a new thread
+    // Creating a new thread
 
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+    pthread_join(thread3, NULL);
+    pthread_join(thread4, NULL);
 }
