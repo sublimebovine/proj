6f7abd75da88f49d1e1f860d1a630e7eb9983531
sublimebovine <sbjohnson211111@gmail.com>
2025-05-07 22:54:52 -0400

    a

--- proj1.c
+++ proj1.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <sys/types.h>
+
 #if __linux
 #include <sys/syscall.h>
 #elif defined(_WIN32) || defined(_WIN64)
@@ -22,12 +23,14 @@ write(STDOUT_FILENO,"hi",2);
 }
 void* proc(void* arg) {
     int sum=0;
-    for(int i=0;i!=11;i++){
+    for(int i=0;i<11;i++){
     pid_t tid = getpid();
     sum=sum+i*tid;
-    write(STDOUT_FILENO,"f",1);
+    //write(STDOUT_FILENO,"i",1);
     sleep(1);
+    
     }
+    
     return NULL;
 }
 
@@ -37,10 +40,20 @@ pthread_t thread1;
 pthread_t thread2;
 pthread_t thread3;
 pthread_t thread4;
+
 pthread_create(&thread1, NULL, proc, NULL);
 pthread_create(&thread2, NULL, proc, NULL);
 pthread_create(&thread3, NULL, proc, NULL);
 pthread_create(&thread4, NULL, proc, NULL);
+printf("%d\n",thread1);
+printf("%d\n",thread2);
+printf("%d\n",thread3);
+printf("%d\n",thread4);
+pthread_join(thread1, NULL);
+pthread_join(thread2, NULL);
+pthread_join(thread3, NULL);
+pthread_join(thread4, NULL);
+
 
 // Creating a new thread
 
