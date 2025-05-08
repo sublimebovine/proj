8db10d37709d2f0d0cd3e589a381f77db6d3cd3b
sublimebovine <sbjohnson211111@gmail.com>
2025-05-07 22:44:38 -0400

    a

--- proj1.c
+++ proj1.c
@@ -1,36 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
 #include <stdarg.h>
-#include <stdio.h> //all includes her
-#include <pthread.h>
 #include <stdio.h>
+#include <pthread.h>
 #include <sys/types.h>
 #if __linux
 #include <sys/syscall.h>
 #elif defined(_WIN32) || defined(_WIN64)
 #include <windows.h>       // Or something like it. 
 #endif
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 
+
+void SIGINT_handler(){
+write(STDOUT_FILENO,"hi",2);
+}
 void* proc(void* arg) {
     int sum=0;
     for(int i=0;i!=11;i++){
     pid_t tid = getpid();
     sum=sum+i*tid;
+    write(STDOUT_FILENO,"f",1);
     sleep(1);
     }
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
+signal(SIGINT, SIGINT_handler);
 pthread_t thread1;
 pthread_t thread2;
 pthread_t thread3;
@@ -39,6 +41,7 @@ pthread_create(&thread1, NULL, proc, NULL);
 pthread_create(&thread2, NULL, proc, NULL);
 pthread_create(&thread3, NULL, proc, NULL);
 pthread_create(&thread4, NULL, proc, NULL);
+
 // Creating a new thread
 
 }
